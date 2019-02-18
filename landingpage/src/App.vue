e98dfa20c7036ade4f5a8d1abb71c1270012f6d9
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2019-02-18 12:38:26 +0100

    landing page first try

---
+++ landingpage/src/App.vue
@@ -0,0 +1,242 @@
+<template>
+  <v-app id="app" :dark="dark">
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      clipped
+      overflow
+      app
+    >
+    <div class="px-4 pt-4">
+      <h4>Search Package by Name</h4>
+      <v-autocomplete
+        v-model="primaryDrawer.pkgpsearchmodel"
+        :items="['Atom', 'DifferentialEquations']"
+        label="Package"
+      ></v-autocomplete>
+    </div>
+    <div class="px-4 pt-3">
+      <h4>Filter by Tag</h4>
+      <v-chip small close>julia</v-chip>
+      <v-chip small close>ide</v-chip>
+    </div>
+    </v-navigation-drawer>
+    <v-toolbar clipped-left app>
+      <v-img
+        src="https://julialang.org/v2/img/logo.svg"
+        contain
+        max-width="70px"
+        height="50px"
+      >
+      </v-img>
+      <v-toolbar-title>Documentation</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-text-field
+        placeholder="Search all the docs..."
+        prepend-inner-icon="search"
+        solo
+        single-line
+        flat
+      ></v-text-field>
+      <!-- <v-toolbar-items>
+        <v-text-field
+          placeholder="Search..."
+          full-width
+        ></v-text-field>
+      </v-toolbar-items> -->
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid v-for="pkg in pkgs">
+        <v-layout align-center justify-center>
+          <v-flex xs10 >
+            <PackageCard :details="pkg"></PackageCard>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer app height="50px">
+      <span class="px-3">&copy; {{ new Date().getFullYear() }} JuliaComputing, Inc</span>
+      <v-spacer></v-spacer>
+      <div class="px-3">
+        <v-switch v-model="dark" primary label="Dark"></v-switch>
+      </div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import PackageCard from './components/PackageCard.vue'
+
+export default {
+  name: 'app',
+  components: {
+    PackageCard,
+  },
+  data: () => ({
+    dark: false,
+    drawers: ['Default (no property)', 'Permanent', 'Temporary'],
+    primaryDrawer: {
+      model: null,
+      pkgpsearchmodel: null,
+    },
+    pkgs: [
+      {
+        name: "DifferentialEquations.jl",
+        owner: "JuliaDiffEq",
+        cistatus: "passing",
+        stars: 707,
+        lastversion: "v6.3.0",
+        tagline: "Julia suite for high-performance solvers of differential equations.",
+        tags: [
+          "differential-equations",
+          "differentialequations",
+          "julia",
+          "ode",
+          "sde",
+          "pde",
+          "dae",
+          "stochastic",
+          "dde",
+          "spde",
+          "delay",
+          "monte-carlo-simulation",
+          "stochastic-processes",
+          "stochastic-differential-equations",
+          "delay-differential-equations",
+          "partial-differential-equations",
+          "differential-algebraic-equations",
+          "simulation",
+          "numerical-integration",
+          "dynamical-systems ",
+        ]
+      },
+      {
+        name: "Atom.jl",
+        owner: "JunoLab",
+        cistatus: "failing",
+        stars: 201,
+        lastversion: "v0.7.14",
+        tagline: "Julia Client for Atom.",
+        tags: ["atom", "ide"]
+      },
+      {
+        name: "DifferentialEquations.jl",
+        owner: "JuliaDiffEq",
+        cistatus: "passing",
+        stars: 707,
+        lastversion: "v6.3.0",
+        tagline: "Julia suite for high-performance solvers of differential equations.",
+        tags: [
+          "differential-equations",
+          "differentialequations",
+          "julia",
+          "ode",
+          "sde",
+          "pde",
+          "dae",
+          "stochastic",
+          "dde",
+          "spde",
+          "delay",
+          "monte-carlo-simulation",
+          "stochastic-processes",
+          "stochastic-differential-equations",
+          "delay-differential-equations",
+          "partial-differential-equations",
+          "differential-algebraic-equations",
+          "simulation",
+          "numerical-integration",
+          "dynamical-systems ",
+        ]
+      },
+      {
+        name: "Atom.jl",
+        owner: "JunoLab",
+        cistatus: "failing",
+        stars: 201,
+        lastversion: "v0.7.14",
+        tagline: "Julia Client for Atom.",
+        tags: ["atom", "ide"]
+      },
+      {
+        name: "DifferentialEquations.jl",
+        owner: "JuliaDiffEq",
+        cistatus: "passing",
+        stars: 707,
+        lastversion: "v6.3.0",
+        tagline: "Julia suite for high-performance solvers of differential equations.",
+        tags: [
+          "differential-equations",
+          "differentialequations",
+          "julia",
+          "ode",
+          "sde",
+          "pde",
+          "dae",
+          "stochastic",
+          "dde",
+          "spde",
+          "delay",
+          "monte-carlo-simulation",
+          "stochastic-processes",
+          "stochastic-differential-equations",
+          "delay-differential-equations",
+          "partial-differential-equations",
+          "differential-algebraic-equations",
+          "simulation",
+          "numerical-integration",
+          "dynamical-systems ",
+        ]
+      },
+      {
+        name: "Atom.jl",
+        owner: "JunoLab",
+        cistatus: "failing",
+        stars: 201,
+        lastversion: "v0.7.14",
+        tagline: "Julia Client for Atom.",
+        tags: ["atom", "ide"]
+      },
+      {
+        name: "DifferentialEquations.jl",
+        owner: "JuliaDiffEq",
+        cistatus: "passing",
+        stars: 707,
+        lastversion: "v6.3.0",
+        tagline: "Julia suite for high-performance solvers of differential equations.",
+        tags: [
+          "differential-equations",
+          "differentialequations",
+          "julia",
+          "ode",
+          "sde",
+          "pde",
+          "dae",
+          "stochastic",
+          "dde",
+          "spde",
+          "delay",
+          "monte-carlo-simulation",
+          "stochastic-processes",
+          "stochastic-differential-equations",
+          "delay-differential-equations",
+          "partial-differential-equations",
+          "differential-algebraic-equations",
+          "simulation",
+          "numerical-integration",
+          "dynamical-systems ",
+        ]
+      },
+      {
+        name: "Atom.jl",
+        owner: "JunoLab",
+        cistatus: "failing",
+        stars: 201,
+        lastversion: "v0.7.14",
+        tagline: "Julia Client for Atom.",
+        tags: ["atom", "ide"]
+      },
+    ]
+  })
+}
+</script>
---
+++ landingpage/src/components/PackageCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-card>
+    <div :class="'package-card ' + details.cistatus">
+      <v-card-title primary-title>
+        <div>
+          <span>
+            <h3 class="headline mb-0">
+              <span class="pkg-owner">
+                {{details.owner}}
+              </span>
+              <span class="pkg-owner">
+                /
+              </span>
+              <span class="pkg-name">
+                {{details.name}}
+              </span>
+            </h3>
+          </span>
+        </div>
+      </v-card-title>
+
+      <v-card-text>
+        {{details.tagline}}
+      </v-card-text>
+      <v-card-text>
+        <v-chip small v-for="tag in details.tags.slice(0, 8)">
+          {{tag}}
+        </v-chip>
+      </v-card-text>
+
+      <v-card-actions>
+        <span class="pl-2">{{details.stars}}</span>
+        <v-icon small>star</v-icon>
+        <span class="pl-2">⋅</span>
+        <span class="pl-2">{{details.lastversion}}</span>
+        <span class="pl-2">⋅</span>
+        <span class="pl-2">3 weeks ago</span>
+        <v-spacer/>
+        <v-btn flat color="grey">Documentation</v-btn>
+        <v-btn flat color="grey">Source</v-btn>
+        <v-btn flat color="grey">Github</v-btn>
+      </v-card-actions>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PackageCard',
+  props: {
+    details: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.package-card {
+  border-left: 0.8em solid grey;
+  &.passing {
+    border-left-color: green;
+  }
+  &.failing {
+    border-left-color: red;
+  }
+
+  .pkg-owner {
+    color: grey;
+  }
+}
+
+</style>
