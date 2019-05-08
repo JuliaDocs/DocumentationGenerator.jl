7c1fa06f77d1bf182e7e3b905aaa35fdbc6d57cd
vdayanand <venkatesh@juliacomputing.com>
2019-05-08 13:57:56 +0530

    add Code/Searc UIs

---
+++ landingpage/src/components/CodefilterCard.vue
@@ -0,0 +1,53 @@
+<template>
+	<v-card>
+		<v-card-title class="pb-0">
+			<a 
+			class="a-color"
+			:href="'https://github.com/' + data.owner + '/' + data.pkgurl + '#L' + data.line"
+			v-html="data.temp_pkgname"
+			target="_blank">
+			</a>
+		</v-card-title>
+		<v-card-text >
+			<code>
+				<span>{{ data.line }}</span>
+				&nbsp;
+				<span v-html="$options.filters.highlight(data.text, searchKey)">{{ data.text }}</span>
+			</code>
+		</v-card-text>
+	</v-card>
+</template>
+<script>
+export default{
+	name: 'CodefilterCard',
+	props: {
+		data: Object,
+		searchKey: String
+	},
+	filters:{
+		highlight (words, query){
+			var str = new RegExp(query, "ig");
+			return words.replace(str, (matchedStr) => {
+				return ('<span style="background-color: #EFD469">' + matchedStr + '</span>');
+			});
+		}
+	}
+};
+</script>
+<style scoped>
+code{
+	padding: 10px;
+	white-space: normal;
+	font-weight: 500;
+	font-size: 90%;
+	width: 100%;
+}
+.theme--dark .a-color{
+	color: #82b1ff;
+}
+.theme--dark code {
+	background-color: transparent;
+	color: white;
+}
+
+</style>
---
+++ landingpage/src/components/DocfilterCard.vue
@@ -0,0 +1,49 @@
+<template>
+	<v-layout>	
+		<ul>
+			<li>
+				<a class="a-color" :href="data.docsfullpath + '/' + data.docname" target="_blank" v-html="$options.filters.highlight(data.title, searchKey)">
+					{{ data.title }}
+				</a>
+				({{data.category}})&nbsp;-&nbsp;<a class="a-color" :href="data.docsfullpath" target="_blank">{{data.pkgname}}</a>
+			</li>
+		</ul>
+	</v-layout>
+</template>
+<script>
+export default{
+	name: 'DocfilterCard',
+	props: {
+		data: Object,
+		searchKey: String
+	},
+	filters:{
+		highlight (words, query){
+			var str = new RegExp(query, "ig");
+			return words.replace(str, (matchedStr) => {
+				return ('<span style="background-color: #EFD469">' + matchedStr + '</span>');
+			});
+		}
+	}
+};
+</script>
+<style scoped>
+code{
+	padding: 10px;
+	white-space: normal;
+	font-weight: 500;
+	font-size: 90%;
+}
+a{
+	text-decoration: none;
+}
+li{
+	font-size: 16px;
+}
+.theme--dark .a-color{
+	color: #82b1ff;
+}
+.theme--dark .highlight{
+	background-color: gray;
+}
+</style>
