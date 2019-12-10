abc9aaa4cbe8a1ecf1ae92bc276267a0fff3876c
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2019-12-10 12:56:00 +0100

    only update registry

--- src/DocumentationGenerator.jl
+++ src/DocumentationGenerator.jl
@@ -72,7 +72,7 @@ function build_documentation(
     process_queue = []
 
     # make sure registry is updated *before* we start multiple processes that might try that at the same time
-    Pkg.update()
+    Pkg.Registry.update()
 
     for package in packages
             # make sure we're not queueing new processes over the limit
