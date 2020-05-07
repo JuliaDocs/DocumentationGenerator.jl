fbe272c0735f81079be21c5e4683a7a3e307e6ae
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2020-05-07 13:07:16 +0200

    better xvfb invocation

--- src/utils/misc.jl
+++ src/utils/misc.jl
@@ -9,3 +9,12 @@ end
 
 get_docs_dir(name, uuid) = get_docs_dir(name, UUID(uuid))
 get_docs_dir(name, uuid::UUID) = joinpath(name, Base.package_slug(uuid, 5))
+
+function find_free_x_servernum()
+    i = 0
+    while isfile("/tmp/.X$(i)-lock")
+        i += 1
+    end
+
+    return i
+end
