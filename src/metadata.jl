1721e5cee45841231631f980cc16f785b4774fc1
Venkatesh Dayananda <venkatesh@juliacomputing.com>
2022-09-14 16:54:14 +0530

    Add ability to inspect exception if fetching repo metadata fails (#178)
    * add ability to inspect exception if fetching repo metadata fails
    
    * one more
    
    * minor fix
    
    * fix it
    
    * fix: pkgeval parsing'

--- src/metadata.jl
+++ src/metadata.jl
@@ -1,7 +1,7 @@
 using GitHub
 
 function init_metadata(packagespec, url)
-    return Dict(
+    meta = Dict(
         "name" => packagespec.name,
         "uuid" => packagespec.uuid,
         "slug" => Base.package_slug(packagespec.uuid, 5),
@@ -12,20 +12,23 @@ function init_metadata(packagespec, url)
         "success" => false,
         "documenter_errored" => missing
     )
-end
-
-function package_metadata(packagespec, url)
-    meta = init_metadata(packagespec, url)
-
     rmatch = match(r".*/(.*)/(.*?(?:\.jl)?)(?:.git)?$", url)
     if rmatch !== nothing
         meta["owner"] = rmatch[1]
         meta["name"] = rmatch[2]
     end
-
-    merge!(meta, update_metadata(packagespec, url, rmatch[1], rmatch[2]))
-
     return meta
+end
+
+function package_metadata(packagespec, url)
+    meta = init_metadata(packagespec, url)
+    new_metadata = try
+        update_metadata(packagespec, url, meta["owner"], meta["name"])
+    catch ex
+        @error(string("Couldn't get info for ", url), error = ex)
+        Dict()
+    end
+    return merge!(meta, new_metadata)
 end
 
 function update_metadata(packagespec, url, repo_owner, repo_name)
@@ -53,18 +56,14 @@ function update_metadata(packagespec, url, repo_owner, repo_name)
         return meta
     end
     @info("Querying metadata.")
-    try
-        gh_auth = authenticate(token)
-        repo_info = repo(repo_owner * "/" * repo_name, auth = gh_auth)
-        meta["description"] = something(repo_info.description, "")
-        meta["stargazers_count"]  = something(repo_info.stargazers_count, 0)
-        meta["homepage"]  = string(something(repo_info.homepage, ""))
-        topics_dict, page = topics(repo_info, auth = gh_auth)
-        meta["tags"] = something(topics_dict["names"], [])
-        meta["contributors"] = contributor_user.(contributors(repo_info, auth = gh_auth)[1])
-    catch err
-        @error(string("Couldn't get info for ", url), error = err)
-    end
+    gh_auth = authenticate(token)
+    repo_info = repo(repo_owner * "/" * repo_name, auth = gh_auth)
+    meta["description"] = something(repo_info.description, "")
+    meta["stargazers_count"]  = something(repo_info.stargazers_count, 0)
+    meta["homepage"]  = string(something(repo_info.homepage, ""))
+    topics_dict, page = topics(repo_info, auth = gh_auth)
+    meta["tags"] = something(topics_dict["names"], [])
+    meta["contributors"] = contributor_user.(contributors(repo_info, auth = gh_auth)[1])
     @info("Done querying metadata.")
 
     return meta
