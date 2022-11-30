4b1854b37be26554d81fe3aa6bc507f6a4c56fb1
Venkatesh Dayananda <venkatesh@juliacomputing.com>
2022-11-30 16:20:33 +0530

    Preserve current working directory

--- src/registry.jl
+++ src/registry.jl
@@ -13,8 +13,9 @@ function get_registry(basepath; registry=DOCS_REGISTRY, sync = true)
     if sync
         try
             rm(joinpath(basepath, "DocumentationGeneratorRegistry"), force = true, recursive = true)
-            cd(basepath)
-            run(`git clone --depth=1 $(registry) DocumentationGeneratorRegistry`)
+            cd(basepath) do
+                run(`git clone --depth=1 $(registry) DocumentationGeneratorRegistry`)
+            end
             @assert isfile(tomlpath)
             return tomlpath
         catch err
